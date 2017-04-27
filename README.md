421cee2d542042b96ddb777a4065bb1c368a7063
veb3 <vincent.balbarin@yale.edu>
2017-04-27 15:38:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Yale ITS ActiveDirectory Manager
+
+This Ruby class uses the `net-ldap` gem to connect to Yale's ActiveDirectory and manage directory objects.
+
+## Usage
+
+Install `bundler`:
+
+```
+gem install bundler
+
+```
+
+Install the application's gems:
+
+```
+bundle install 
+```
+
+Create the file `.env` to  hold the authentication information required  to bind to ActiveDirectory
+
+```
+cp .env.template .env
+```
+
+Change the values for the ActiveDirectory account name and its password:
+
+```
+sed -i 's/__svc_acct_dn__/ Service account DistinguishedName /g'
+sed -i 's/__svc_pass__/ Password, escaping any special characters /g'
+```
+
+(The resulting `.env` will be ignored by `git`.)
+
+The file `example.rb.txt` demonstrates the use of the class and its methods. Create a copy of the file as `example.rb`, make the modifications inside the file, make executable, and run:
+
+```
+bundle exec dotenv ./example.rb
+```
+
+The `dotenv` gem will load the authentication information inside `.env` into the Ruby execution environment and run `example.rb`
+
+
+## Version 0.1.0
+
+* `#dn`: Retrieves the DistinguishedName of an AD object possessing the specified sAMAccountName.
+* `#create_computer`: Creates a new computer object in the specified OU and sets the managedBy attribute to the sAMAccountName of the owner.
+* `#delete_computer`: Deletes the computer object matching the specified sAMAccountName.
---
+++ yaleits_ad_manager.rb
@@ -0,0 +1,69 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'net/ldap'
+
+class YaleitsAdManager
+  SERVER = 'ad.its.yale.edu'
+  PORT = 636
+  BASE_DN = 'dc=yu,dc=yale,dc=edu'
+  DOMAIN = 'yu.yale.edu'
+
+  def connect(account, password)
+    return nil if account.empty? or password.empty?
+
+    connection =
+      Net::LDAP.new(
+        { :host        => SERVER,
+          :port        => PORT,
+          :encryption  => { :method      => :simple_tls,
+                            :tls_options =>
+                             OpenSSL::SSL::SSLContext::DEFAULT_PARAMS},
+          :auth        => { :method      => :simple,
+                            :username    => account,
+                            :password    => password } }
+    )
+    connection.bind && connection || nil
+
+  rescue Net::LDAP::LdapError => e
+    return nil
+  end
+
+  def delete_computer(ad_account)
+    account_dn = dn(ad_account)
+    @ad_connection.delete(:dn => account_dn) unless account_dn.nil?
+  end
+
+  def create_computer(owner_id, ad_account, ad_ou)
+    account_dn = "CN=#{ad_account},#{ad_ou}"
+    owner_id_dn = dn(owner_id)
+    account_attrs = {
+      :cn              => ad_account,
+      :sAMAccountName  => "#{ad_account}$",
+      :objectClass     => [ 'computer', 'organizationalPerson', 'person',
+                            'top',      'user' ],
+      :managedBy       => owner_id_dn
+    }
+    results = {}
+    @ad_connection.add(
+      :dn         => account_dn,
+      :attributes => account_attrs
+    )
+    results[0] = @ad_connection.get_operation_result
+    puts results[0]
+  end
+
+  def dn(ad_account)
+    l_filter = Net::LDAP::Filter.eq("sAMAccountName", "#{ad_account}")
+    r_filter = Net::LDAP::Filter.eq("sAMAccountName", "#{ad_account}$")
+    filter = Net::LDAP::Filter.intersect(l_filter, r_filter)
+    entry = @ad_connection.search(
+      :base   => BASE_DN,
+      :filter => filter
+    ).first
+    entry && entry.dn || nil 
+  end
+
+  def initialize(account, password)
+    @ad_connection = connect(account, password)
+  end
+end
