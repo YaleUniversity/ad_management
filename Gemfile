01213437593d4b3304239ce92c8dd7cd0ae033a4
veb3 <vincent.balbarin@yale.edu>
2017-05-18 16:24:14 +0000

    Converted to module

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
-source "https://rubygems.org"
 
-gem 'net-ldap'
+source 'https://rubygems.org'
+
 gem 'dotenv'
-
-# gem "rails"
+gem 'net-ldap'
--- yaleits_ad_manager.rb
+++ yaleits_ad_manager.rb
@@ -2,33 +2,36 @@ require 'rubygems'
 require 'bundler/setup'
 require 'net/ldap'
 
-# YaleitsAdManager: manages Yale University ActiveDirectory objects
-class YaleitsAdManager
+# YaleitsAdManager manages Yale University ActiveDirectory objects
+module  YaleitsAdManager
+  # Establish defaults for Yale University ActiveDirectory environment
   SERVER = 'ad.its.yale.edu'.freeze
   PORT = 636
   BASE_DN = 'dc=yu,dc=yale,dc=edu'.freeze
   DOMAIN = 'yu.yale.edu'.freeze
 
-  def connect(account, password)
+  @ad_connection = nil
+
+  def self.connect(account, password)
     return nil if account.empty? || password.empty?
 
-    connection = Net::LDAP.new(
+    @ad_connection = Net::LDAP.new(
       host: SERVER, port: PORT,
       auth:       { username: account, password: password, method: :simple },
       encryption: { method:     :simple_tls,
                     tls_options: OpenSSL::SSL::SSLContext::DEFAULT_PARAMS }
     )
-    connection.bind && connection || nil
+    @ad_connection.bind ? @ad_connection : nil
   rescue Net::LDAP::LdapError
     return nil
   end
 
-  def delete_computer(ad_account)
+  def self.delete_computer(ad_account)
     account_dn = dn(ad_account)
     @ad_connection.delete(dn: account_dn) unless account_dn.nil?
   end
 
-  def create_computer(owner_id, ad_account, ad_ou)
+  def self.create_computer(owner_id, ad_account, ad_ou)
     account_dn = "CN=#{ad_account},#{ad_ou}"
     owner_id_dn = dn(owner_id)
     account_attrs = {
@@ -40,7 +43,7 @@ class YaleitsAdManager
     @ad_connection.add(dn: account_dn, attributes: account_attrs)
   end
 
-  def dn(ad_account)
+  def self.dn(ad_account)
     l_filter = Net::LDAP::Filter.eq('sAMAccountName', ad_account)
     r_filter = Net::LDAP::Filter.eq('sAMAccountName', ad_account + '$')
     filter = Net::LDAP::Filter.intersect(l_filter, r_filter)
@@ -49,9 +52,5 @@ class YaleitsAdManager
       filter:  filter
     ).first
     entry && entry.dn || nil
-  end
-
-  def initialize(account, password)
-    @ad_connection = connect(account, password)
   end
 end
