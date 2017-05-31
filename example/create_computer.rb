868d1adec7f7cc625791a27f5c20f7a237a6fdb5
veb3 <vincent.balbarin@yale.edu>
2017-05-31 20:19:57 +0000

    Broke out examples

---
+++ example/create_computer.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'ad_management'
+
+## Replace with specific values
+conputer_cn      = '__ComputerCN__'
+owner_id         = '__ActiveDirectoryUserAccount__'
+computer_ou_dn   = '__ComputerOUDistinguishedName__'
+
+## Create a new ad_manager instance using service account credentials
+
+AdManagement.configure_from('./config/connection_settings.yml')
+AdManagement.connect
+
+## Create a new computer account in ActiveDirectory
+AdManagement.create_computer(owner_id, conputer_cn, computer_ou_dn)
--- example/get_computer_dn.rb
+++ example/get_computer_dn.rb
@@ -3,21 +3,15 @@
 require 'ad_management'
 
 ## Replace with specific values
-computer_account = '__ComputerAccoutName__'
+conputer_cn      = '__ComputerCN__'
 owner_id         = '__ActiveDirectoryUserAccount__'
 computer_ou_dn   = '__ComputerOUDistinguishedName__'
 
+
 ## Create a new ad_manager instance using service account credentials
 
-AdManagement.configure_from('config/connection_settings.yml')
+AdManagement.configure_from('./config/connection_settings.yml')
 AdManagement.connect
 
 ## Display a computer accounts DistinguishedName
 puts AdManagement.dn_from(conputer_cn + '$')
-
-
-## Delete a computer account in ActiveDirectory
-# AdManagement.delete_computer(conputer_cn)
-
-## Create a new computer account in ActiveDirectory
-# AdManagement.create_computer(owner_id, conputer_cn, computer_ou_dn)
