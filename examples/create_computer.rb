f1115ad35d75d036b8de02ec7119f564b493b501
veb3 <vincent.balbarin@yale.edu>
2017-06-06 12:06:15 -0400

    Fixed typos

--- examples/create_computer.rb
+++ examples/create_computer.rb
@@ -3,7 +3,7 @@
 require 'ad_management'
 
 ## Replace with specific values
-conputer_cn      = '__ComputerCN__'
+computer_cn      = '__ComputerCN__'
 owner_id         = '__ActiveDirectoryUserAccount__'
 computer_ou_dn   = '__ComputerOUDistinguishedName__'
 
@@ -13,4 +13,4 @@ AdManagement.configure_from('./config/connection_settings.yml')
 AdManagement.connect
 
 ## Create a new computer account in ActiveDirectory
-AdManagement.create_computer(owner_id, conputer_cn, computer_ou_dn)
+AdManagement.create_computer(owner_id, computer_cn, computer_ou_dn)
--- examples/get_computer_dn.rb
+++ examples/get_computer_dn.rb
@@ -3,7 +3,7 @@
 require 'ad_management'
 
 ## Replace with specific values
-conputer_cn      = '__ComputerCN__'
+computer_cn      = '__ComputerCN__'
 owner_id         = '__ActiveDirectoryUserAccount__'
 computer_ou_dn   = '__ComputerOUDistinguishedName__'
 
@@ -14,4 +14,4 @@ AdManagement.configure_from('./config/connection_settings.yml')
 AdManagement.connect
 
 ## Display a computer accounts DistinguishedName
-puts AdManagement.dn_from(conputer_cn + '$')
+puts AdManagement.dn_from(computer_cn + '$')
